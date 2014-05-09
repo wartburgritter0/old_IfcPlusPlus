1123dd18f852b9b9931a5815076eef6a70e6892a
Bernd Hahnebach <bernd@bimstatik.org>
2014-05-09 22:41:40 +0200

    IfcPP and IfcPPGeometry: Fix: changed name space of shared_ptr to std:: since C++11 is used

--- IfcPlusPlus/src/ifcpp/model/shared_ptr.h
+++ IfcPlusPlus/src/ifcpp/model/shared_ptr.h
@@ -31,11 +31,11 @@ using std::tr1::enable_shared_from_this;
 
 #elif defined __GNUC__ && !defined(__FreeBSD__)
 
-#include <tr1/memory>
-using std::tr1::shared_ptr;
-using std::tr1::weak_ptr;
-using std::tr1::dynamic_pointer_cast;
-using std::tr1::enable_shared_from_this;
+#include <memory>
+using std::shared_ptr;
+using std::weak_ptr;
+using std::dynamic_pointer_cast;
+using std::enable_shared_from_this;
 
 #define _stricmp strcasecmp
 
